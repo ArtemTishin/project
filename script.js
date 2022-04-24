c07f28e7bcfb553ff3128cd8128ed3fe83888b5f
Artem <tema.arttem@yandex.ru>
2022-04-24 08:27:53 +0300

    project

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 "use strict";
 
-const user = prompt("What a you name?");
-alert(`Hello, ${user}`);
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+
+let MovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genters: [],
+    privat: false
+
+};
+
+const a = prompt("Один из последних фильмов?");
+const b = prompt("На сколько оцените его?");
+const c = prompt("Один из последних фильмов?");
+const d = prompt("На сколько оцените его?");
+
+MovieDB.movies[a]=b;
+MovieDB.movies[c]=d;
+
+console.log(MovieDB);
